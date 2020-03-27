82ca4b7c0716ddb66b7ea1e88095036eafe24c16
mdhussain418 <47389603+mdhussain418@users.noreply.github.com>
2020-03-27 17:15:12 +0530

    Create RSASSA_PKCS_1_VERIFY.py

---
+++ RSASSA_PKCS_1_VERIFY.py
@@ -0,0 +1,19 @@
+from Crypto.Signature import PKCS1_PSS
+from Crypto.Hash import SHA256
+import base64
+from Crypto.PublicKey.RSA import construct
+import binascii
+
+### generating public key from exponent and modulus
+e = long('10001', 16)
+n = int('83A73A4FBF....', 16)
+
+key = construct((n, e))
+
+message = 'rsassa_pkcs_1.py'
+h = SHA256.new(message)
+print(h.hexdigest())
+
+signature = base64.decodestring('base64 encoded value of signature..')
+result = PKCS1_PSS.new(key, saltLen=16).verify(h, signature)
+print(result)
