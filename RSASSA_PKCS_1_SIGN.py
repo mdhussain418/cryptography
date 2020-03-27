499ccb164e8123fda6bb00305dc602fb1a9c1298
mdhussain418 <47389603+mdhussain418@users.noreply.github.com>
2020-03-27 17:15:42 +0530

    Update RSASSA_PKCS_1_SIGN.py

--- RSASSA_PKCS_1_SIGN.py
+++ RSASSA_PKCS_1_SIGN.py
@@ -19,5 +19,5 @@ h = SHA256.new(message)
 print(h.hexdigest())
 
 signature = PKCS1_PSS.new(key, saltLen=16).sign(h)
-base64EncodedStr = base64.b64encode(signature)
-print(base64EncodedStr)
+base64EncodedSignature = base64.b64encode(signature)
+print(base64EncodedSignature)
